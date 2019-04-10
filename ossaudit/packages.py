3baf0f1c60f5d13611d5824258749be0a6ef13b5
Hans Jerry Illikainen <hji@dyntopia.com>
2019-04-10 08:23:10 +0000

    packages: add get_from_files()

---
+++ ossaudit/packages.py
@@ -0,0 +1,120 @@
+# Copyright (c) 2019, Hans Jerry Illikainen <hji@dyntopia.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+from typing import IO, List
+
+import dparse
+import packaging.version
+
+
+class Package:
+    def __init__(self, name: str, version: str) -> None:
+        self._name = name
+        self._version = version
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def version(self) -> str:
+        return self._version
+
+    @property
+    def coordinate(self) -> str:
+        return "pkg:pypi/{name}@{version}".format(
+            name=self.name, version=self.version
+        )
+
+
+class _Version(packaging.version.Version):  # type: ignore
+    def __iadd__(self, num: int) -> "_Version":
+        """
+        Add a number to the release.
+        """
+        release = self.release[:-1] + (self.release[-1] + num, )
+        self._version = self._version._replace(release=release)  # type: ignore
+        return self
+
+    def __isub__(self, num: int) -> "_Version":
+        """
+        Subtract a number from the release.
+
+        For example:
+
+        - 2.5.0 is decreased to 2.4.9
+        - 2018.0 is decreased to 2017.9
+
+        This is obviously quite horrible.  We don't really know (without
+        consulting PyPI, which we don't) if these versions are actually
+        correct.  The previous versions could just as well be:
+
+        - 2.4.123 or 2.4.3
+        - 2017.12 or 2016.1
+
+        This functionality is meant to decrease the version number for
+        pre-releases (since OSS Index breaks on them), so imperfections
+        are probably alright.
+        """
+        for _ in range(num):
+            release = list(reversed(self.release))
+            for i, x in enumerate(release):
+                x -= 1
+                release[i] = x if x >= 0 else 9
+                if x >= 0:
+                    self._version = self._version._replace(
+                        release=tuple(reversed(release))
+                    )
+                    break
+        return self
+
+
+def get_from_files(fhs: List[IO[str]]) -> List[Package]:
+    """
+    Read packages from a list of file handles.
+
+    The version specifier(s) for a package can be written in a number of
+    ways:
+
+        pkg
+        pkg == 0.1
+        pkg > 1, <= 3
+        pkg == 1.0pre
+        pkg == 1.0pre1.dev2
+        pkg > 1!2rc3.dev6
+        pkg ===abcd
+        pkg != 3
+        pkg == 3.2.*
+        pkg ~= 1.2
+
+    Also, the `packaging` module accepts bogus version specifiers such
+    as:
+
+        pkg >5, <4, ==9, ==1
+
+    For now, the lowest value of the operators ==, ~=, > and >= is used
+    as the coordinate version for OSS Index.  Upper bounds and negations
+    are ignored.
+
+    Furthermore, only the `final release` part of the version is used
+    (see PEP 440).  The reason is that OSS Index doesn't seem to handle
+    other parts of a version specifier very well.  For example, the
+    coordinate `pkg:pypi/django@1.7.2` gives a number of vulnerabilities
+    that `pkg:pypi/django@1.7.2rc1` is missing.
+    """
+    pkgs = []
+    for f in fhs:
+        for dep in dparse.parse(f.read(), path=f.name).dependencies:
+            versions = []
+            for spec in dep.specs:
+                if spec.operator in ["==", "~=", ">=", ">"]:
+                    version = _Version(spec.version.replace("*", "0"))
+                    if version.is_prerelease:
+                        version -= 1
+                    if spec.operator == ">":
+                        version += 1
+                    versions.append(version)
+            version = min(versions or [_Version("0")])
+            pkgs.append(Package(dep.name, version.base_version))
+    return pkgs
---
+++ tests/test_packages.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2019, Hans Jerry Illikainen <hji@dyntopia.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# pylint: disable=protected-access
+
+import io
+import json
+from unittest import TestCase
+
+from ossaudit import packages
+
+
+class TestVersion(TestCase):
+    def test_arithmetic(self) -> None:
+        versions = [
+            ("0", 1, "1", "0"),
+            ("0", 123, "123", "0"),
+            ("0.0.0", 1, "0.0.1", "0.0.0"),
+            ("0.0.9", 9, "0.0.18", "0.0.0"),
+            ("1.1.1", 1, "1.1.2", "1.1.0"),
+            ("1.1.1", 5, "1.1.6", "1.0.6"),
+            ("1.16.1", 1, "1.16.2", "1.16.0"),
+            ("100", 1, "101", "99"),
+            ("9.9.10", 1, "9.9.11", "9.9.9"),
+            ("9.9.9", 1, "9.9.10", "9.9.8"),
+        ]
+
+        for before, num, add, sub in versions:
+            v = packages._Version(before)
+            v += num
+            self.assertEqual(v.base_version, add)
+
+            v = packages._Version(before)
+            v -= num
+            self.assertEqual(v.base_version, sub)
+
+
+class TestGetFromFiles(TestCase):
+    def test_requirements(self) -> None:
+        pkgs = [
+            ("django == 2.*", "django@2.0"),
+            ("mccabe >= 9.9.9rc1", "mccabe@9.9.8"),
+            ("munch >= 9.9.10rc1", "munch@9.9.9"),
+            ("mypy >= 10.10.10rc1", "mypy@10.10.9"),
+            ("numpy > 1.16.1, > 1.15.4, == 1.16.0", "numpy@1.15.5"),
+            ("packaging == 2.0.0b3", "packaging@1.9.9"),
+            ("pkg-resources == 1.3.0", "pkg-resources@1.3.0"),
+            ("pycodestyle == 1.3.0b2", "pycodestyle@1.2.9"),
+            ("pydocstyle > 1.2.9", "pydocstyle@1.2.10"),
+            ("pyflakes > 0.0.0b3", "pyflakes@0.0.1"),
+            ("pyparsing >= 0b2", "pyparsing@0"),
+            ("pytz ~= 2018.9", "pytz@2018.9"),
+            ("pyyaml > 5.1b5, < 8", "pyyaml@5.1"),
+            ("requests == 2.21.0", "requests@2.21.0"),
+            ("scipy == 1.*.1", "scipy@1.0.1"),
+            ("scrapy >= 1.6.0", "scrapy@1.6.0"),
+            ("setuptools < 5", "setuptools@0"),
+            ("six == 1.2.3 --hash=sha256:abcd", "six@1.2.3"),
+            ("snowballstemmer == 0.0.0b3", "snowballstemmer@0.0.0"),
+            ("twisted", "twisted@0"),
+            ("yapf >= 5.1b5", "yapf@5.0"),
+        ]
+        f = io.StringIO("\n".join(p for p, _ in pkgs))
+        f.name = "requirements.txt"
+
+        got = [p.coordinate for p in packages.get_from_files([f])]
+        want = ["pkg:pypi/{}".format(p) for _, p in pkgs]
+        self.assertEqual(sorted(got), sorted(want))
+
+    def test_pipfile(self) -> None:
+        pkgs = [
+            ("docutils = '> 0.6.5rc5'", "docutils@0.6.5"),
+            ("flake8 = '==0.6.5'", "flake8@0.6.5"),
+            ("pysocks = '>= 0.6.5rc5'", "pysocks@0.6.4"),
+            ("pytest = '>=2.8.0,<4.1'", "pytest@2.8.0"),
+            ("pytest-mock = '>2.8.0,<4.1'", "pytest-mock@2.8.1"),
+            ("requests = '>5.1, >=4.2, >=3.3, >9.4'", "requests@3.3"),
+            ("sphinx = '==0.6.5rc5'", "sphinx@0.6.4"),
+            ("tox = '*'", "tox@0"),
+        ]
+        f = io.StringIO("[packages]\n{}".format("\n".join(p for p, _ in pkgs)))
+        f.name = "Pipfile"
+
+        got = [p.coordinate for p in packages.get_from_files([f])]
+        want = ["pkg:pypi/{}".format(p) for _, p in pkgs]
+        self.assertEqual(sorted(got), sorted(want))
+
+    def test_pipfile_lock(self) -> None:
+        pkgs = [
+            ("docutils", "> 0.6.5rc5", "docutils@0.6.5"),
+            ("pytest", "==2.8.0", "pytest@2.8.0"),
+        ]
+
+        entries = {n: {"version": v, "hashes": []} for n, v, *_ in pkgs}
+        f = io.StringIO(json.dumps({"default": entries}))
+        f.name = "Pipfile.lock"
+
+        got = [p.coordinate for p in packages.get_from_files([f])]
+        want = ["pkg:pypi/{}".format(p) for *_, p in pkgs]
+        self.assertEqual(sorted(got), sorted(want))
+
+    def test_tox(self) -> None:
+        pkgs = [
+            ("pylint", "pylint@0"),
+            ("pytest >= 3.0.0, <4", "pytest@3.0.0"),
+            ("pytz ~= 2018.9", "pytz@2018.9"),
+        ]
+
+        f = io.StringIO("[x]\ndeps={}".format("\n ".join(p for p, _ in pkgs)))
+        f.name = "tox.ini"
+
+        got = [p.coordinate for p in packages.get_from_files([f])]
+        want = ["pkg:pypi/{}".format(p) for _, p in pkgs]
+        self.assertEqual(sorted(got), sorted(want))
+
+    def test_multiple(self) -> None:
+        r = [
+            ("pytz > 2018.9", "pytz@2018.10"),
+            ("requests == 1.2", "requests@1.2"),
+        ]
+        rf = io.StringIO("\n".join(x for x, _ in r))
+        rf.name = "requirements.txt"
+
+        p = [
+            ("pysocks = '>= 0.6.5rc5'", "pysocks@0.6.4"),
+            ("pytest = '>2.8.0'", "pytest@2.8.1"),
+        ]
+        pf = io.StringIO("[packages]\n{}".format("\n".join(x for x, _ in p)))
+        pf.name = "Pipfile"
+
+        t = [
+            ("docutils >3.0", "docutils@3.1"),
+            ("pylint", "pylint@0"),
+        ]
+        tf = io.StringIO("[x]\ndeps={}".format("\n ".join(x for x, _ in t)))
+        tf.name = "tox.ini"
+
+        got = [p.coordinate for p in packages.get_from_files([rf, pf, tf])]
+        want = ["pkg:pypi/{}".format(x) for x in [y for *_, y in r + p + t]]
+        self.assertEqual(sorted(got), sorted(want))
